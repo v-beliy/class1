89a5e864fefa4ea8f8ab52be0543981263a5e86d
Vasyl Bieliakovskyi <v.belyakovsky@gmail.com>
2024-05-06 16:12:18 +0100

    WA1204-09 [VBIEL] Add versions and variables

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "5.45.0"
-        }
-    }
-}
-
 provider "aws" {
     region = "eu-central-1"
+}
+
+provider "gitlab" {
+  base_url = var.gitlab_url
+  token = var.gitlab_token
 }
